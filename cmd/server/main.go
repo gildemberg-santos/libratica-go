fe4e2cbad1166bcd0e4ae0634f293cbf79f404bd
gildemberg-santos <gildemberg.santos@gmail.com>
2024-07-21 10:15:22 -0300

    feat: Add HTTP server functionality to main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World! (Server)")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World! (Server)")
+	})
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
